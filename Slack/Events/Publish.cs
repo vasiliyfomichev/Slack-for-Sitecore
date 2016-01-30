c59f68bff1fc6647b089e1f88d90f6018256784c
Vasiliy Fomichev <vasya.fomichev@gmail.com>
2016-01-29 19:48:42 -0800

    Added lib folder for sitecore references and started work on publisher.

---
+++ Slack/Events/Publish.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Slack.Events
+{
+    public class Publish
+    {
+        public void OnPublishEnd(object sender, EventArgs args)
+        {
+            //Publisher publisher = Event.ExtractParameter(args, 0) as Publisher;
+        }
+    }
+}
