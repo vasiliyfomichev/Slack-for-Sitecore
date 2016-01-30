2046e4e89b0396d62f02f72ec14b222d45b663fc
Volodymyr Lukashevych <v.lukashevych@globallogic.com>
2016-01-29 23:35:46 -0600

    added mappings for templates

---
+++ Slack/Templates/EventType.cs
@@ -0,0 +1,14 @@
+﻿using Sitecore.Data;
+
+namespace Slack.Templates
+{
+    public struct EventType
+    {
+        public static readonly ID ID = new ID("{C63F7736-70D1-447C-8142-7D581C664CE1}");
+
+        public struct Fields
+        {
+            public static readonly ID Name = new ID("{FA66CE6A-17DA-483D-B707-03B2962ACC86}");
+        }
+    }
+}
---
+++ Slack/Templates/Publication.cs
@@ -0,0 +1,17 @@
+﻿using Sitecore.Data;
+
+namespace Slack.Templates
+{
+    public struct Publication
+    {
+        public static readonly ID ID = new ID("{E8BD1697-9A84-4D18-91D1-4FC9F8594B6E}");
+
+        public struct Fields
+        {
+            public static readonly ID Name = new ID("{AD560039-1BAB-4136-BB25-597E5C706C39}");
+            public static readonly ID Description = new ID("{9432B5E6-1DDB-41E7-B4D3-5FCF9B63F6BD}");
+
+            public static readonly ID Channels = new ID("{99AB54D0-2D15-4654-84C4-3AA806E5DC7B}");
+        }
+    }
+}
