851e6cb6614685b09c4898468bea452d1ec404b5
Vasiliy Fomichev <vasya.fomichev@gmail.com>
2016-01-29 20:05:22 -0800

    Defined slack message.

--- Slack/Contracts/ISlackMessage.cs
+++ Slack/Contracts/ISlackMessage.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Slack.Contracts
+﻿namespace Slack.Contracts
 {
+    /// <summary>
+    /// Message object to be posted to Slack Web API
+    /// https://api.slack.com/methods/chat.postMessage
+    /// </summary>
     internal interface ISlackMessage
     {
+        string Token { get; set; }
+        string Channel { get; set; }
+        string Text { get; set; }
+        string Username { get; set; }
+        bool AsUser { get; set; }
+        string IconUrl { get; set; }
     }
 }
---
+++ Slack/Models/SlackMessage.cs
@@ -0,0 +1,14 @@
+﻿using Slack.Contracts;
+
+namespace Slack.Models
+{
+    public class SlackMessage : ISlackMessage
+    {
+        public string Token { get; set; }
+        public string Channel { get; set; }
+        public string Text { get; set; }
+        public string Username { get; set; }
+        public bool AsUser { get; set; }
+        public string IconUrl { get; set; }
+    }
+}
